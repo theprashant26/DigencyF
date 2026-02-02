649f775ae94c515cef842591784b1ba28967951f
Prashant Kumar <prashantsingh9826@gmail.com>
2026-02-03 01:36:36 +0530

    Updated all pages

--- js/script.js
+++ js/script.js
@@ -118,7 +118,7 @@ function initThemeSwitch() {
             $('body').addClass('lightmode');
             localStorage.setItem('lightmode', 'active');
 
-            siteLogos.attr('src', 'image/marko-logo-dark.png');
+            siteLogos.attr('src', 'image/digiencylogo.png');
 
             partnerLogos.each(function () {
                 const $img = $(this);
@@ -131,7 +131,7 @@ function initThemeSwitch() {
             $('body').removeClass('lightmode');
             localStorage.removeItem('lightmode');
 
-            siteLogos.attr('src', 'image/marko-logo.png');
+            siteLogos.attr('src', 'image/digiencylogow.png');
 
             partnerLogos.each(function () {
                 const $img = $(this);
@@ -341,52 +341,52 @@ $(document).ready(function(){
     const data = [
         {
             title: "Home",
-            description: "Amplify Your Brand with Cutting-Edge Digital Marketing Watch our video reviews and see how businesses achieve success with Marko's digital marketing solutions. Marko empowers businesses to grow online with data driven digital marketing, innovative branding, and performance focused strategies trusted by top brands lorem ipsum dolor sit amet consectetur. Get Started 2.7k Positive Reviews Ready [...]",
+            description: "Amplify Your Brand with Cutting-Edge Digital Marketing Solutions ",
             url: "index.html"
         },
         {
             title: "About",
-            description: "About Marko Home / About Us 0 + Years of Experience on Digital Marketing Services About Us Who We Are & What Drives Us At Marko, we specialize in crafting innovative digital marketing strategies that drive real business growth. Our expertise ensures your brand stays ahead in the competitive digital landscape. Get to know the [...]",
+            description: "About Digency Digital Marketing Agency Home / About Us ",
             url: "about.html"
         },
         {
             title: "Services",
-            description: "Our Services Home / Services Our Core Services Digital Solutions That Drive Real Results Social Media Marketing Build brand awareness & engage your audience effectively lorem ipsum dolor sit amet consectetur adip. View Details Content Marketing Build brand awareness & engage your audience effectively lorem ipsum dolor sit amet consectetur adip. View Details PPC Advertising […]",
+            description: "Our Services ",
             url: "service.html"
         },
         {
             title: "Single Services",
-            description: "Social Media Marketing Home / Services Details Our Expertise Boost Your Brand with Strategic Social Media Marketing Maximize engagement, build loyal communities, and drive conversions across all major platforms lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Service Overview At Marko, we help brands grow […]",
+            description: "Social Media Marketing ",
             url: "single_services.html"
         },
         {
             title: "Case Studies",
-            description: "Case Studies Home / Case Studies Case Studies See How We Help Businesses Thrive We don't just talk about results—we deliver them. Here are some of our most impactful case studies showcasing how our digital marketing strategies drive success. More Case Studies Social Influencer Retargeting Google Video Local Community Local Business Digital Transformation 5x ROI […]",
+            description: "Case Studies ",
             url: "case_studies.html"
         },
         {
             title: "Our Team",
-            description: "Meet Our Team Home / Our Team Case Studies Meet the Minds Behind Your Digital Success Ethan Morales Head of Creative Sophia Zhang Senior SEO Specialist Liam Turner Performance Marketing Lead Olivia Bennett Creative Director Daniel White Client Success Manager Chloe Ramirez Social Media Manager Powering Success for Top Brands Lorem ipsum dolor sit amet, […]",
+            description: "Meet Our Team ",
             url: "team.html"
         },
         {
             title: "Partnership",
-            description: "Partnership Home / Partnership Client & Partnership Strong Partnerships, Proven Success See How We Help Brands Grow Transform Your Business with Marko! Take your digital marketing to the next level with data-driven strategies and innovative solutions. Let's create something amazing together! 2.7k Positive Reviews 0 % Improved Project 0 % New Project Social Media Growth […]",
+            description: "Partnership ",
             url: "partnership.html"
         },
         {
             title: "Pricing Plan",
-            description: "Pricing Plan Home / Pricing Plan Our Core Services Flexible Pricing Plans for Every Business Let's Find the Right Strategy for You! Book a Free Consultation Starter Perfect for startups & small businesses $99 / Month View Details Basic SEO & Digital Marketing Social Media Management (1 platform) Monthly Performance Report Enterprise Full scale marketing […]",
+            description: "Pricing Plan ",
             url: "pricing.html"
         },
         {
             title: "Testimonial",
-            description: "Testimonials Home / Testimonials 2.7k Positive Reviews 0 % Improved Project 0 % New Project Social Media Growth Performance Marketing What Our Client Says Hear from Our Satisfied Clients, Real Success Stories Discover how businesses like yours achieved outstanding growth with Marko's expert digital marketing solutions. Emma Richard CEO Nexatech 'Marko completely transformed our online [...]',",
+            description: "Testimonials",
             url: "testimonial.html"
         },
         {
             title: "FAQs",
-            description: "Simple, Direct, and Friendly Home / FAQ Frequently Asked Questions Got Questions? We've Got Answers. What services does Marko offer? We specialize in digital marketing, including branding, social media management, content strategy, paid ads, and analytics-driven campaigns. How long does it take to see results? While some channels like paid ads offer quicker results, most […]",
+            description: "Simple, Direct, and Friendly Home / FAQ Frequently Asked Questions ",
             url: "faq.html"
         },
         {
@@ -396,17 +396,17 @@ $(document).ready(function(){
         },
         {
             title: "Blog",
-            description: "Our Blog Home / Blog Insights & Trends Latest Digital Marketing Strategies & Tips Explore our latest blog articles covering industry trends, expert insights, and actionable strategies to elevate your digital marketing game. View All Articles April 14, 2025 Social Media Mastering Instagram and Facebook Ads Lorem ipsum dolor si consectetur adipiscing elit ut elit […]",
+            description: "Our Blog Home / Blog Insights & Trends Latest Digital Marketing Strategies",
             url: "blog.html"
         },
         {
             title: "Single Post",
-            description: "Growth Strategies for Digital Businesses Home / Single Post Recent Blog April 14, 2025 Mastering Instagram and Facebook Ads April 14, 2025 Growth Strategies for Digital Business Transform Your Business with Marko! Take your digital marketing to the next level with data-driven strategies and innovative solutions. Let's create something amazing together! Read More How to […]",
+            description: "Growth Strategies for Digital Businesses ",
             url: "single_post.html"
         },
         {
             title: "Contact Us",
-            description: "Contact Us Home / Contact Us Reach out to us Get in Touch Reach out to us for tailored digital solutions that drive results sollicitudin nec. Phone Number +1 (62) 987 7543 Email Address hello@markoagency.com Office Address Marko HQ - 902 Digital Lane, San Francisco, CA 94110, USA",
+            description: "Contact Us Home / Contact Us ",
             url: "contact.html"
         },
     ];
